4bdf7b4de245f7c6b5c28014d1964a6bd2b4c4f2
ElGautinho <gcherubala11@gmail.com>
2026-07-05 21:43:20 +0200

    uc-login

--- Arnold/cypress/e2e/UC-Login/login.cy.js
+++ Arnold/cypress/e2e/UC-Login/login.cy.js
@@ -1,60 +1,84 @@
-describe("Login page", () => {
+describe("Login Page", () => {
 
   beforeEach(() => {
-    // Navigate to the application homepage
+    // Open the application homepage
     cy.visit("https://student.michaelkentburns.com");
 
     // Accept the cookie consent banner
-    cy.get('.cky-notice-btn-wrapper > .cky-btn-accept').click({ force: true });
+    cy.get(".cky-notice-btn-wrapper > .cky-btn-accept").click({ force: true });
 
-    // Verify that the homepage has loaded successfully
-    cy.url().should('include', 'student.michaelkentburns.com');
+    // Verify that the homepage is loaded
+    cy.url().should("include", "student.michaelkentburns.com");
 
-    // Open the User menu
-    cy.contains('User').click();
-
-    // Navigate to the Login page
-    cy.contains('Login').click();
+    // Navigate to the login page
+    cy.contains("User").click();
+    cy.contains("Login").click();
   });
 
-  it("should reject unknown username", () => {
+  it("should reject an unknown username", () => {
 
-    // Enter a username that does not exist
+    // Enter an unknown username
     cy.get('[name="log"]')
-      .should('be.visible')
+      .should("be.visible")
       .clear()
-      .type('king', { delay: 50 })
-      .should('have.value', 'king');
+      .type("king", { delay: 50 });
 
     // Enter a password
     cy.get('[name="pwd"]')
-      .should('be.visible')
+      .should("be.visible")
       .clear()
-      .type('user123', { delay: 50 })
-      .should('have.value', 'user123');
+      .type("user123", { delay: 50 });
+
+    // Enable the Remember Me option
+    cy.get('[name="rememberme"]').check({ force: true });
 
     // Submit the login form
     cy.get('[name="wp-submit"]').click();
+
   });
 
-  it("should reject wrong password", () => {
+  it("should reject an invalid password", () => {
 
     // Enter a valid username
     cy.get('[name="log"]')
-      .should('be.visible')
+      .should("be.visible")
       .clear()
-      .type('leonce', { delay: 50 })
-      .should('have.value', 'leonce');
+      .type("Arnold", { delay: 50 });
 
-    // Enter an invalid password
+    // Enter an incorrect password
     cy.get('[name="pwd"]')
-      .should('be.visible')
+      .should("be.visible")
       .clear()
-      .type('wrongpassword', { delay: 50 })
-      .should('have.value', 'wrongpassword');
+      .type("wrongpassword", { delay: 50 });
+
+    // Enable the Remember Me option
+    cy.get('[name="rememberme"]').check({ force: true });
 
     // Submit the login form
     cy.get('[name="wp-submit"]').click();
+
+  });
+
+  it("should login successfully with valid credentials", () => {
+
+    // Enter a valid username
+    cy.get('[name="log"]')
+      .should("be.visible")
+      .clear()
+      .type("Arnold", { delay: 50 });
+
+    // Enter the correct password
+    cy.get('[name="pwd"]')
+      .should("be.visible")
+      .clear()
+      .type("Lephare-bukavu0970005782", { delay: 50 });
+
+    // Enable the Remember Me option
+    cy.get('[name="rememberme"]').check({ force: true });
+
+    // Submit the login form
+    cy.get('[name="wp-submit"]').click();
+
   });
 
 });
